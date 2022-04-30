50ebfdd06599cc2ac137cd8bbcb24dae8f1a9ee9
naamagi <n.gilat@gmail.com>
2022-04-30 18:41:44 +0300

    update

--- game.js
+++ game.js
@@ -24,12 +24,12 @@ let matrix = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0],
-  [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0],
-  [0, 0, 0, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 4, 0, 0, 0],
-  [0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 4, 0, 0, 3],
+  [0, 0, 0, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0],
+  [0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 0, 0],
+  [0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0],
+  [0, 0, 0, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0],
+  [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 4, 0, 0, 0],
+  [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 4, 0, 0, 3],
   [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
   [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
   [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
@@ -42,31 +42,47 @@ const generateBoard = () => {
   for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 20; j++) {
       const cell = document.createElement("div");
-      cell.setAttribute("activeTile","false");
+    //   cell.addEventListener("click", pickTile);
       switch (matrix[i][j]) {
         case 0:
           cell.classList.add("sky");
+          cell.setAttribute("filledTile", "false");
+
           break;
         case 1:
           cell.classList.add("cloud");
+          cell.setAttribute("filledTile", "false");
+
           break;
         case 2:
           cell.classList.add("dirt");
+          cell.setAttribute("filledTile", "true");
+
           break;
         case 3:
           cell.classList.add("rock");
+          cell.setAttribute("filledTile", "true");
+
           break;
         case 4:
           cell.classList.add("trunk");
+          cell.setAttribute("filledTile", "true");
+
           break;
         case 5:
           cell.classList.add("leaves");
+          cell.setAttribute("filledTile", "true");
+
           break;
         case 6:
           cell.classList.add("grass");
+          cell.setAttribute("filledTile", "true");
+
           break;
         case 7:
           cell.classList.add("sun");
+          cell.setAttribute("filledTile", "false");
+
           break;
         default:
       }
@@ -79,13 +95,31 @@ const generateBoard = () => {
 
 generateBoard();
 
-const addFunctionalityToTools = () => {
-  const axe = document.getElementById("axe");
-  const pickaxe = document.getElementById("pickaxe");
-  const shovel = document.getElementById("shovel");
-axe.addEventListener("click",(event) => {
-
-})
-
-
+const initiateTools = () => {
+  const tools = document.querySelectorAll("#game-tools");
+  for (const tool of tools) {
+    tool.setAttribute("activeTool", "false");
+    tool.addEventListener("click", changeFunctionalityToTools);
+  }
+  //   const tool = event.target;
 };
+
+initiateTools();
+
+const changeFunctionalityToTools = (event) => {
+  const pickedTool = event.target;
+  const tools = document.querySelectorAll("#game-tools");
+  for (const tool of tools) {
+    if (tool.activeTool) {
+      tool.setAttribute("activeTool", "false");
+    }
+  }
+  pickedTool.setAttribute("activeTool", "true");
+};
+
+
+// const pickTile=(event) => {
+//     const pickedTile = event.target;
+// if(pickedTile.filledTile)
+
+// }
