678f0489ed6fa746ccc842dabdffb95aaefb91c5
naamagi <n.gilat@gmail.com>
2022-05-01 00:24:10 +0300

    update

--- game.js
+++ game.js
@@ -42,69 +42,55 @@ const generateBoard = () => {
   for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 20; j++) {
       const cell = document.createElement("div");
-    //   cell.addEventListener("click", pickTile);
       switch (matrix[i][j]) {
         case 0:
           cell.classList.add("sky");
-          cell.setAttribute("filledTile", "false");
+          cell.setAttribute("pickableTile", "false");
 
           break;
         case 1:
           cell.classList.add("cloud");
-          cell.setAttribute("filledTile", "false");
+          cell.setAttribute("pickableTile", "false");
 
           break;
         case 2:
           cell.classList.add("dirt");
-          cell.setAttribute("filledTile", "true");
+          cell.setAttribute("pickableTile", "true");
 
           break;
         case 3:
           cell.classList.add("rock");
-          cell.setAttribute("filledTile", "true");
+          cell.setAttribute("pickableTile", "true");
 
           break;
         case 4:
           cell.classList.add("trunk");
-          cell.setAttribute("filledTile", "true");
+          cell.setAttribute("pickableTile", "true");
 
           break;
         case 5:
           cell.classList.add("leaves");
-          cell.setAttribute("filledTile", "true");
+          cell.setAttribute("pickableTile", "true");
 
           break;
         case 6:
           cell.classList.add("grass");
-          cell.setAttribute("filledTile", "true");
+          cell.setAttribute("pickableTile", "true");
 
           break;
         case 7:
           cell.classList.add("sun");
-          cell.setAttribute("filledTile", "false");
+          cell.setAttribute("pickableTile", "false");
 
           break;
         default:
       }
       gameBoard.appendChild(cell);
-      //draw div
-      //div class="cell"
     }
   }
 };
 
 generateBoard();
-
-const initiateTools = () => {
-  const tools = document.querySelectorAll("#game-tools");
-  for (const tool of tools) {
-    tool.setAttribute("activeTool", "false");
-    tool.addEventListener("click", changeFunctionalityToTools);
-  }
-  //   const tool = event.target;
-};
-
-initiateTools();
 
 const changeFunctionalityToTools = (event) => {
   const pickedTool = event.target;
@@ -116,10 +102,55 @@ const changeFunctionalityToTools = (event) => {
   }
   pickedTool.setAttribute("activeTool", "true");
 };
+const initiateTools = () => {
+  const tools = document.querySelectorAll("#game-tools");
+  for (const tool of tools) {
+    tool.setAttribute("activeTool", "false");
+    tool.addEventListener("click", changeFunctionalityToTools);
+  }
+  //   const tool = event.target;
+};
 
+initiateTools();
+const pickTile = (event) => {
+  const pickedTile = event.target;
+  const tools = document.querySelector("#game-tools");
+  console.log(`${pickedTile.className}`);
+  const className = pickedTile.className;
+  const matchingTool = tools.querySelector(`.${className}`);
 
-// const pickTile=(event) => {
-//     const pickedTile = event.target;
-// if(pickedTile.filledTile)
+  console.log("matchingtool:" + matchingTool.className);
+  const chosenTool = tools.querySelector('div[activeTool="true"]');
+  console.log("chosenTool:" + chosenTool.className);
 
-// }
+  const bank = tools.querySelector("#bank");
+  console.log("bank:" + bank.id);
+  console.log("pickedTile.pickableTile:" + pickedTile.getAttribute('pickableTile'));
+  console.log("matchingTool.activeTool:" + matchingTool.getAttribute('activeTool'));
+  console.log("pickedTile.className:" + pickedTile.className);
+ 
+  if (pickedTile.getAttribute('pickableTile')) {
+    if (matchingTool.getAttribute('activeTool')) {
+      bank.classList.add(`${className}`);
+        pickedTile.classList.add("sky");
+      pickedTile.pickableTile = "false";
+      console.log("pickedTile.pickableTile:" + pickedTile.pickableTile);
+
+      //   bank.setAttribute("background", pickedTile.background);
+      //   pickedTile.setAttribute("background, ")
+    }
+    //     else{
+    // tool
+    //     }
+  }
+};
+
+const addingFunctionalitiesToTiles = () => {
+  const gameBoard = document.getElementById("game-board");
+  const cells = gameBoard.children;
+  for (const cell of cells) {
+    cell.addEventListener("click", pickTile);
+  }
+};
+
+addingFunctionalitiesToTiles();
